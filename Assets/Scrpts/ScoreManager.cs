d337a053ca7267a55d82b179c55b07691d01f05e
adrian montalto <adrianmontalto2@gmail.com>
2016-06-21 23:44:18 +1000

    increasead drunk factor with score

--- Assets/Scrpts/ScoreManager.cs
+++ Assets/Scrpts/ScoreManager.cs
@@ -7,6 +7,8 @@ public class ScoreManager : MonoBehaviour
 {
     public Text player1ScoreText;
     public Text player2ScoreText;
+    public Drunkeness player1;
+    public Drunkeness player2;
 
     private int player1Score = 0;
     private int player2Score = 0;
@@ -31,11 +33,13 @@ public class ScoreManager : MonoBehaviour
     public void AddPlayer1Score(int score)
     {
         player1Score += score;
+        player1.SetDrunkFactor(1.0f);
     }
 
     public void Addplayer2Score(int score)
     {
         player2Score += score;
+        player2.SetDrunkFactor(1.0f);
     }
 
     void GoToMenu()
