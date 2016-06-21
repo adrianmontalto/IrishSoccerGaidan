6187a457bc466737c24214c59bfd4aa0a543eff3
Adrian Montalto <adrian.montalto@ad.aie.edu>
2016-06-21 12:39:36 +1000

    got drunk movement

---
+++ Assets/Scrpts/Drunkeness.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drunkeness : MonoBehaviour
+{
+    private CharacterController controller;
+    private float xFactor = 0.0f;
+    private float yFactor = 0.0f;
+    private float drunkFactor = 1.0f;
+    private float movementTimer = 0.0f;//a timer to calculate movement
+    private float speed = 0.0f;
+    public Player player;
+    public float movementResetTimer = 0.0f;//value to reset timer
+    private float lerpTime = 1.0f;
+    public float randomMin = 0.0f;
+    public float randomMax = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        xFactor = Random.Range(randomMin * drunkFactor,randomMax * drunkFactor);
+        yFactor = Random.Range(randomMin * drunkFactor,randomMax * drunkFactor);
+        speed = player.speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        movementTimer -= Time.deltaTime;
+        if(movementTimer < 0)
+        {
+            xFactor = Random.Range(randomMin * drunkFactor, randomMax * drunkFactor);
+            yFactor = Random.Range(randomMin * drunkFactor, randomMax * drunkFactor);
+            movementTimer = movementResetTimer;
+        }
+        float x =Mathf.Lerp(0.0f, xFactor, Mathf.Clamp((lerpTime / 10.0f), 0.0f, 10.0f));
+        float y =Mathf.Lerp(0.0f, yFactor, Mathf.Clamp((lerpTime / 10.0f), 0.0f, 10.0f));
+        Vector3 direction = new Vector3(x,0,y);
+        controller.Move(direction * speed * Time.deltaTime); 
+	}
+
+    public void SetDrunkFactor(float increase)
+    {
+        drunkFactor += increase;
+    }
+}
--- Assets/Scrpts/Player.cs
+++ Assets/Scrpts/Player.cs
@@ -5,18 +5,13 @@ public class Player : MonoBehaviour
 {
     private CharacterController controller;//the character controller
     private float gravityForce = 0.0f;//the force of gravity
-    private float movementTimer = 0.0f;//a timer to calculate movement
-    private int drunkFactor = 0;//a number for how drunk the player is
     private int controllerNumber = 0;//the number for the assigned controller
     private bool isActive = false;//to determine whether the player can move
     [Range(1, 2)]
     public int playerNumber = 0;//the players number
-    public float movementResetTimer = 0.0f;//value to reset timer
     [Range(1.0f,10.0f)]
     public float speed = 0.0f;//the players speed
     public float gravity = 20.0f;//the players gravity
-    public float randomMin = 0.0f;//the min for the player random movement
-    public float randomMax = 0.0f;//the max for the player random movement
     
     // Use this for initialization
     void Start()
@@ -57,7 +52,7 @@ public class Player : MonoBehaviour
 
     void ControllerInputCheck(float x, float y,string horizontal,string vertical)
     {
-        movementTimer -= Time.deltaTime;
+        
         //checks to see whether the player is moving up or down and left or right
         float h_input = x * speed;
         float v_input = y * speed;
@@ -74,25 +69,6 @@ public class Player : MonoBehaviour
 
         //moves the player
         Vector3 directions = new Vector3(h_input, gravityForce, v_input);
-
-        if (movementTimer < 0)
-        {
-            //checks to see if the up or down button are pressed
-            if (Input.GetButton(horizontal))
-            {
-                float random = Random.Range(randomMin, randomMax);
-                directions = new Vector3(h_input + random, gravityForce, v_input);
-                directions.Normalize();
-                movementTimer = movementResetTimer;
-            }
-            if (Input.GetButton(vertical))
-            {
-                float random = Random.Range(randomMin, randomMax);
-                directions = new Vector3(h_input, gravityForce, v_input + random);
-                directions.Normalize();
-                movementTimer = movementResetTimer;
-            }
-        }
 
         controller.Move(directions * Time.deltaTime);
         //checks to see if the player is grounded
@@ -116,8 +92,6 @@ public class Player : MonoBehaviour
 
     void Player1KeyboardCheck()
     {
-        movementTimer -= Time.deltaTime;
-        
         //checks to see whether the player is moving up or down and left or right
         float h_input = Input.GetAxis("K1Horizontal") * speed;
         float v_input = Input.GetAxis("K1Vertical") * speed;
@@ -135,25 +109,6 @@ public class Player : MonoBehaviour
         //moves the player
         Vector3 directions = new Vector3(h_input, gravityForce, v_input);
         
-        if (movementTimer < 0)
-        {
-            //checks to see if the up or down button are pressed
-            //if (Input.GetButton("K1Horizontal"))
-            //{
-            //    float random = Random.Range(randomMin, randomMax);
-            //    directions = new Vector3(h_input + random, gravityForce, v_input);
-            //    directions.Normalize();
-            //    movementTimer = movementResetTimer;
-            //}
-            //if (Input.GetButton("K1Vertical"))
-            //{
-            //    float random = Random.Range(randomMin, randomMax);
-            //    directions = new Vector3(h_input, gravityForce, v_input + random);
-            //    directions.Normalize();
-            //    movementTimer = movementResetTimer;
-            //}
-        }
-        
         controller.Move(directions * Time.deltaTime);
         //checks to see if the player is grounded
         if (controller.isGrounded)
@@ -165,8 +120,6 @@ public class Player : MonoBehaviour
 
     void Player2KeyboardCheck()
     {
-        movementTimer -= Time.deltaTime;
-
         float h_input = Input.GetAxis("K2Horizontal") * speed;
         float v_input = Input.GetAxis("K2Vertical") * speed;
 
@@ -182,25 +135,6 @@ public class Player : MonoBehaviour
         
         //moves the player
         Vector3 directions = new Vector3(h_input, gravityForce, v_input);
-        
-        if (movementTimer < 0)
-        {
-            //checks to see if the up or down button are pressed
-            if (Input.GetButton("K2Horizontal"))
-            {
-                float random = Random.Range(randomMin, randomMax);
-                directions = new Vector3(h_input + random, gravityForce, v_input);
-                directions.Normalize();
-                movementTimer = movementResetTimer;
-            }
-            if (Input.GetButton("K2Vertical"))
-            {
-                float random = Random.Range(randomMin, randomMax);
-                directions = new Vector3(h_input, gravityForce, v_input + random);
-                directions.Normalize();
-                movementTimer = movementResetTimer;
-            }
-        }
         
         controller.Move(directions * Time.deltaTime);
         //checks to see if the player is grounded
