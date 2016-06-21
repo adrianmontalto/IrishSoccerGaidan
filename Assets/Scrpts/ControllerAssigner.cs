5c54290961705209278c6de8e412dedd74148aaa
Adrian Montalto <adrian.montalto@ad.aie.edu>
2016-06-21 10:06:41 +1000

    added two controllers

---
+++ Assets/Scrpts/ControllerAssigner.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerAssigner : MonoBehaviour
+{
+    private bool isController1Available = true;
+    private bool isController2Available = true;
+
+    private bool isPlayer1Set = false;
+    private bool isPlayer2Set = false;
+
+    public GameObject player1;
+    public GameObject player2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckWhichController();
+    }
+
+    void CheckWhichController()
+    {
+        //checks if player 1 is available
+        if (isPlayer1Set == false)
+        {
+            SetPlayer(player1, ref isPlayer1Set);
+        }
+
+        //checks if player 2 is available
+        if (isPlayer2Set == false)
+        {
+            if (isPlayer1Set)
+            {
+                SetPlayer(player2, ref isPlayer2Set);
+            }
+        }        
+    }
+
+    void SetPlayer(GameObject player, ref bool setPlayer)
+
+    {
+        bool isSet = false;
+        if (!isSet)
+        {
+            //checks if controller 1 is available
+            if (isController1Available)
+            {
+                //checks to see if the start button is pressed
+                if (Input.GetButton("J1Start"))
+                {
+                    isController1Available = false;
+                    player.GetComponent<Player>().SetControllerNumber(1);
+                    player.GetComponent<Player>().SetActive(true);
+                    setPlayer = true;
+                }
+            }
+
+            //checks if controller 2 is available
+            if (isController2Available)
+            {
+                //checks to see if the start button is pressed
+                if (Input.GetButton("J2Start"))
+                {
+                    isController2Available = false;
+                    player.GetComponent<Player>().SetControllerNumber(2);
+                    player.GetComponent<Player>().SetActive(true);
+                    setPlayer = true;
+                }
+            }
+        }
+    }
+}
--- Assets/Scrpts/Player.cs
+++ Assets/Scrpts/Player.cs
@@ -7,6 +7,10 @@ public class Player : MonoBehaviour
     private float gravityForce = 0.0f;//the force of gravity
     private float movementTimer = 0.0f;//a timer to calculate movement
     private int drunkFactor = 0;//a number for how drunk the player is
+    private int controllerNumber = 0;//the number for the assigned controller
+    private bool isActive = false;//to determine whether the player can move
+    [Range(1, 2)]
+    public int playerNumber = 0;//the players number
     public float movementResetTimer = 0.0f;//value to reset timer
     [Range(1.0f,10.0f)]
     public float speed = 0.0f;//the players speed
@@ -21,13 +25,45 @@ public class Player : MonoBehaviour
         controller = gameObject.GetComponent<CharacterController>();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        //checks to see if the player is active
+        if(isActive == true)
+        {
+            string axisname = "J" + controllerNumber + "Horizontal";
+            float xAxis = Input.GetAxis(axisname);
+            string horizontalAxis = axisname;
+            axisname = "J" + controllerNumber + "Vertical";
+            float yAxis = Input.GetAxis(axisname);
+            string verticalAxis = axisname;
+            ControllerInputCheck(xAxis,yAxis,horizontalAxis,verticalAxis);
+        }
+        InputCheck();
+    }
+
     //checks what keys the player is pressing
     void InputCheck()
     {
+        if(playerNumber == 1)
+        {
+            Player1KeyboardCheck();
+        }
+        if(playerNumber == 2)
+        {
+            Player2KeyboardCheck();
+        }
+    }
+
+    void ControllerInputCheck(float x, float y,string horizontal,string vertical)
+    {
         movementTimer -= Time.deltaTime;
         //checks to see whether the player is moving up or down and left or right
-        float h_input = Input.GetAxis("Horizontal") * speed;
-        float v_input = Input.GetAxis("Vertical") * speed;
+        float h_input = x * speed;
+        float v_input = y * speed;
+
+        Vector3 facingDirection = new Vector3(h_input, 0, v_input);
+        transform.LookAt(transform.position + facingDirection);
 
         //checks to see if the player isn't grounded
         if (!controller.isGrounded)
@@ -38,17 +74,18 @@ public class Player : MonoBehaviour
 
         //moves the player
         Vector3 directions = new Vector3(h_input, gravityForce, v_input);
-        
-        if(movementTimer < 0)
+
+        if (movementTimer < 0)
         {
-            if (Input.GetButton("Up") || Input.GetButton("Down"))
+            //checks to see if the up or down button are pressed
+            if (Input.GetButton(horizontal))
             {
                 float random = Random.Range(randomMin, randomMax);
                 directions = new Vector3(h_input + random, gravityForce, v_input);
                 directions.Normalize();
                 movementTimer = movementResetTimer;
             }
-            if (Input.GetButton("Left") || Input.GetButton("Right"))
+            if (Input.GetButton(vertical))
             {
                 float random = Random.Range(randomMin, randomMax);
                 directions = new Vector3(h_input, gravityForce, v_input + random);
@@ -66,10 +103,111 @@ public class Player : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetActive(bool active)
     {
-        //calls the input check function
-        InputCheck();
+        //sets the active bool
+        isActive = active;
+    }
+
+    public void SetControllerNumber(int number)
+    {
+        controllerNumber = number;
+    }
+
+    void Player1KeyboardCheck()
+    {
+        movementTimer -= Time.deltaTime;
+        
+        //checks to see whether the player is moving up or down and left or right
+        float h_input = Input.GetAxis("K1Horizontal") * speed;
+        float v_input = Input.GetAxis("K1Vertical") * speed;
+
+        Vector3 facingDirection = new Vector3(h_input, 0, v_input);
+        transform.LookAt(transform.position + facingDirection);
+        
+        //checks to see if the player isn't grounded
+        if (!controller.isGrounded)
+        {
+            //applies gravity to the playe
+            gravityForce += Physics.gravity.y * gravity * Time.deltaTime;
+        }
+        
+        //moves the player
+        Vector3 directions = new Vector3(h_input, gravityForce, v_input);
+        
+        if (movementTimer < 0)
+        {
+            //checks to see if the up or down button are pressed
+            //if (Input.GetButton("K1Horizontal"))
+            //{
+            //    float random = Random.Range(randomMin, randomMax);
+            //    directions = new Vector3(h_input + random, gravityForce, v_input);
+            //    directions.Normalize();
+            //    movementTimer = movementResetTimer;
+            //}
+            //if (Input.GetButton("K1Vertical"))
+            //{
+            //    float random = Random.Range(randomMin, randomMax);
+            //    directions = new Vector3(h_input, gravityForce, v_input + random);
+            //    directions.Normalize();
+            //    movementTimer = movementResetTimer;
+            //}
+        }
+        
+        controller.Move(directions * Time.deltaTime);
+        //checks to see if the player is grounded
+        if (controller.isGrounded)
+        {
+            //makes the jumpforce = nothing
+            gravityForce = 0.0f;
+        }
+    }
+
+    void Player2KeyboardCheck()
+    {
+        movementTimer -= Time.deltaTime;
+
+        float h_input = Input.GetAxis("K2Horizontal") * speed;
+        float v_input = Input.GetAxis("K2Vertical") * speed;
+
+        Vector3 facingDirection = new Vector3(h_input, 0, v_input);
+        transform.LookAt(transform.position + facingDirection);
+        
+        //checks to see if the player isn't grounded
+        if (!controller.isGrounded)
+        {
+            //applies gravity to the playe
+            gravityForce += Physics.gravity.y * gravity * Time.deltaTime;
+        }
+        
+        //moves the player
+        Vector3 directions = new Vector3(h_input, gravityForce, v_input);
+        
+        if (movementTimer < 0)
+        {
+            //checks to see if the up or down button are pressed
+            if (Input.GetButton("K2Horizontal"))
+            {
+                float random = Random.Range(randomMin, randomMax);
+                directions = new Vector3(h_input + random, gravityForce, v_input);
+                directions.Normalize();
+                movementTimer = movementResetTimer;
+            }
+            if (Input.GetButton("K2Vertical"))
+            {
+                float random = Random.Range(randomMin, randomMax);
+                directions = new Vector3(h_input, gravityForce, v_input + random);
+                directions.Normalize();
+                movementTimer = movementResetTimer;
+            }
+        }
+        
+        controller.Move(directions * Time.deltaTime);
+        //checks to see if the player is grounded
+        if (controller.isGrounded)
+        {
+            //makes the jumpforce = nothing
+            gravityForce = 0.0f;
+        }
     }
 }
